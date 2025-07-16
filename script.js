8ee91f083a1e0baf59852d38f4d6dddf654f2cad
Thejas A <thejas2246@gmail.com>
2025-07-16 07:37:27 +0530

    add password validation

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const form = document.querySelector("form");
+
 const emailInput = document.querySelector("#email");
 const emailError = document.querySelector(".email-error");
+
+const passwordInput = document.querySelector("#password");
+const passwordError = document.querySelector(".password-error");
 
 emailInput.addEventListener("input", () => {
     if (emailInput.validity.valid) {
@@ -9,6 +13,40 @@ emailInput.addEventListener("input", () => {
         showEmailError();
     }
 });
+
+passwordInput.addEventListener("input", isPasswordValid);
+
+function isPasswordValid() {
+    let errorMessage = getPasswordError();
+    if (errorMessage.length !== 0) {
+        passwordError.textContent = errorMessage.join(",");
+    } else {
+        passwordError.textContent = "";
+        return true;
+    }
+    return false;
+}
+
+function getPasswordError() {
+    let message = [];
+    if (passwordInput.value.length !== 0) {
+        if (!/[A-Z]/.test(passwordInput.value)) {
+            message.push("one uppercase letter");
+        }
+        if (!/[a-z]/.test(passwordInput.value)) {
+            message.push("one lowerCase letter");
+        }
+        if (!/[^a-zA-Z0-9]/.test(passwordInput.value)) {
+            message.push("one Symbol");
+        }
+        if (!/[0-9]/.test(passwordInput.value)) {
+            message.push("one Number");
+        }
+    } else {
+        message.push("please enter a value");
+    }
+    return message;
+}
 
 function showEmailError() {
     if (emailInput.validity.valueMissing) {
